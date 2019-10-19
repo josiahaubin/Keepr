ac7ccf486d8813a5e67846883038fe8ba07ce7ab
Josiah Aubin <josiahaubin@gmail.com>
2019-10-19 17:02:26 -0600

    Basic modals pop up, no content yet

--- wwwsrc/src/views/Dashboard.vue
+++ wwwsrc/src/views/Dashboard.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="Dashboard">
-    <h1>user dash</h1>
+    <div class="row mt-3">
+      <div class="col-12">
+        <h1>{{user.username}}'s Dashboard</h1>
+      </div>
+    </div>
+    <div class="row mt-3">
+      <div class="col-6">
+        <button
+          class="btn btn-success"
+          type="button"
+          data-toggle="modal"
+          data-target="#CreateKeepModal"
+        >Create a Keep</button>
+        <CreateKeep />
+      </div>
+      <div class="col-6">
+        <button
+          class="btn btn-success"
+          type="button"
+          data-toggle="modal"
+          data-target="#CreateVaultModal"
+        >Create a Vault</button>
+        <CreateVault />
+      </div>
+    </div>
   </div>
 </template>
 
 
 <script>
+import CreateKeep from "../components/CreateKeep";
+import CreateVault from "../components/CreateVault";
 export default {
   name: "Dashboard",
   data() {
     return {};
   },
-  computed: {},
+  computed: {
+    user() {
+      return this.$store.state.user;
+    }
+  },
   methods: {},
-  components: {}
+  components: { CreateKeep, CreateVault }
 };
 </script>
 
