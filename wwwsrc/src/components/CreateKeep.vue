f25f0f183ae6c2b2d074f446226372e6077efac4
Josiah Aubin <josiahaubin@gmail.com>
2019-10-19 17:14:57 -0600

    Keep creator modal set up with form, no submit yet

--- wwwsrc/src/components/CreateKeep.vue
+++ wwwsrc/src/components/CreateKeep.vue
@@ -4,13 +4,38 @@
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title">Modal title</h5>
+            <h5 class="modal-title">Keep Creator</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div class="modal-body">
-            <p>Modal body text goes here.</p>
+            <!-- FORM -->
+            <form>
+              <div class="form-group">
+                <label for="name">Keep Name</label>
+                <input type="text" class="form-control" id="name" placeholder="Name" required />
+              </div>
+              <div class="form-group">
+                <label for="description">Keep Description</label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="description"
+                  placeholder="Description"
+                  required
+                />
+              </div>
+              <div class="form-group">
+                <label for="img">Keep Image</label>
+                <input type="text" class="form-control" id="img" placeholder="Image URL" required />
+              </div>
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value id="privacy" />
+                <label class="form-check-label" for="privacy">Do you want this keep set to private?</label>
+              </div>
+            </form>
+            <!-- END OF FORM -->
           </div>
           <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -37,4 +62,7 @@ export default {
 
 
 <style scoped>
+.form-group {
+  text-align: left;
+}
 </style>
