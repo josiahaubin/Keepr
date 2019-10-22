22478e09f5be3522c8cffc68163bb08f4ba838d5
Josiah Aubin <josiahaubin@gmail.com>
2019-10-22 14:47:38 -0600

    adding to a vault pushes you to that vault

--- wwwsrc/src/store.js
+++ wwwsrc/src/store.js
@@ -193,6 +193,7 @@ export default new Vuex.Store({
       try {
         let res = await api.post("vaultkeeps", payload)
         NotificationService.toast("Successfully added to vault")
+        dispatch('getVaultById', res.data.vaultId)
       } catch (error) {
         NotificationService.toastError("No Vault selected");
         console.log(error)
